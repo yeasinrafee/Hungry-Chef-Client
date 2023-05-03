913d199d7df24f70c28e865bd8af51dd24a62b62
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-03 10:55:33 +0600

    Router setup and footer added

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
