9ad7b4cd018e177e26f27e211be49c4ba24962da
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-04 12:39:58 +0600

    image input field

--- src/pages/Login/Registration/Registration.jsx
+++ src/pages/Login/Registration/Registration.jsx
@@ -5,10 +5,16 @@ import { Link } from "react-router-dom";
 const Registration = () => {
   const handleRegistration = (event) => {
     event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password, name);
   };
   return (
     <div className="form-container">
-      <h2 className="form-title mt-6">Registrar Now</h2>
+      <h2 className="form-title mt-2">Registrar Now</h2>
       <form onSubmit={handleRegistration}>
         <div className="form-control">
           <label htmlFor="name">Name</label>
@@ -21,6 +27,12 @@ const Registration = () => {
         <div className="form-control">
           <label htmlFor="password">Password</label>
           <input type="password" name="password" required />
+        </div>
+        <div className="">
+          <label htmlFor="image" className="block mb-2 text-lg">
+            Image
+          </label>
+          <input type="file" name="image" />
         </div>
         <input className="btn-submit" type="submit" value="Sign Up" />
       </form>
