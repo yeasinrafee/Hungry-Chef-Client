b1cf59b3bcf9a2235ff468c85c5bbbd29c12a3fc
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-04 15:12:33 +0600

    Loader added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,25 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="h-96 w-96 mx-auto my-60">
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+    );
+  }
 
   if (user) {
     return children;
